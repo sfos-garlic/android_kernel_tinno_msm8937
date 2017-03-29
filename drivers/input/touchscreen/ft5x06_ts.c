7e5bbb0cc9deb2837c1a06dc78e7bd07397fd369
haojl2 <haojl2@motorola.com>
2017-03-29 18:06:34 +0800

    input: ft5x06_ts: using FW upgrade framework if enabled
    If focaltech FW upgrade framework is enabled, using this way to
    upgrade FW.
    
    Change-Id: I65dac02f2faccba01b7c95442a6f7a887f1e5047
    Signed-off-by: haojl2 <haojl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/970226
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -36,6 +36,9 @@
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
 #include <linux/proc_fs.h>
+#if defined(CONFIG_TOUCHSCREEN_FOCALTECH_UPGRADE)
+#include "focaltech_flash.h"
+#endif
 
 #if (defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) && !defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE))
 #include <linux/input/doubletap2wake.h>
@@ -2480,13 +2483,21 @@ static ssize_t ft5x06_do_reflash_store(struct device *dev,
 			data->fw_name);
 
 	mutex_lock(&data->input_dev->mutex);
+	ft5x06_irq_disable(data);
 	data->loading_fw = true;
-	retval = ft5x06_fw_upgrade(dev, true);
+#if defined(CONFIG_TOUCHSCREEN_FOCALTECH_UPGRADE)
+		retval = fts_ctpm_auto_upgrade(data->client,
+				data->fw_name,
+				data->pdata);
+#else
+		retval = ft5x06_fw_upgrade(dev, true);
+#endif
 	if (retval)
 		dev_err(&data->client->dev,
 				"%s: FW %s upgrade failed\n",
 				__func__,
 				data->pdata->name);
+	ft5x06_irq_enable(data);
 	data->loading_fw = false;
 	mutex_unlock(&data->input_dev->mutex);
 
