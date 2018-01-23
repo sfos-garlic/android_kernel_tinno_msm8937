aea8de565f9ab6e4b49c2dd8be8e2c8ec7ac4a67
Ryan Andri <ryanandri@linuxmail.org>
2018-01-23 09:25:37 +0700

    input: dt2w/s2w: integrate all fixes into tree.
    - remove dependancy of PREVENT_TO_SLEEP config.
    - force to hardcode instead of module compile.
    - fix screen wakeup when touching home key.
    - fix screen wakeup when gesture occur while incall.
    - fix deep sleep issue.
    - fix screen freeze issue.
    - add dependency to not conflict with TOUCHSCREEN_FT5X06_GESTURE.
    
    note:   actualy the driver ft5x06_ts is support gesture by default.
    	there is TOUCHSCREEN_FT5X06_GESTURE, but not sure it work on santoni.
    	you can it self to try them :)
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- include/linux/input/ft5x06_ts.h
+++ include/linux/input/ft5x06_ts.h
@@ -74,4 +74,8 @@ struct ft5x06_ts_platform_data {
 	int (*power_on)(bool);
 };
 
+#if (defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) || defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE))
+extern bool gesture_incall;
+#endif
+
 #endif
