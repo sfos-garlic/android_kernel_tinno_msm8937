7e7035913b0803a2a6c7d9bfdcab1b08b722f6b7
andip71 <andreasp@gmx.de>
2018-05-11 21:03:18 +0700

    msm: mdss: Adding lcd notifier
    Supports notifier for start&end of LCD power on&off
    
    Author and credits: Se Hun Kim <sean2.kim@lge.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -23,6 +23,7 @@
 #include <linux/regulator/consumer.h>
 #include <linux/leds-qpnp-wled.h>
 #include <linux/clk.h>
+#include <linux/lcd_notify.h>
 #include <linux/uaccess.h>
 #include <linux/msm-bus.h>
 #include <linux/pm_qos.h>
