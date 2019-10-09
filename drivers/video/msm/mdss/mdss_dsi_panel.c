25382d80a432a0326e3e312313a4c695739a670f
yank555-lu <yank555.lu@gmail.com>
2019-10-10 00:37:33 +0530

    kernel/power/powersuspend: new PM kernel driver for Android
    PM kernel driver for Android w/o early suspend v1.5 (faux123/Yank555.lu)
    commit: https://github.com/symkuangyaw/android_kernel_xiaomi_kenzo/commit/b97df40b7f97db69e5c799179b6626f9db10f0c3

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -29,6 +29,11 @@
 #ifdef TARGET_HW_MDSS_HDMI
 #include "mdss_dba_utils.h"
 #endif
+
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -1019,6 +1024,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 end:
 	/* clear idle state */
